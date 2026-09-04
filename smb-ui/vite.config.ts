91b15dad50efdaaf71ea3b571c30d0ba289220ec
Raheel Nadeem <raheeljiofficial@gmail.com>
2026-09-04 23:49:17 +0500

    feat: configure Vite with React, Tailwind CSS, and path aliasing

--- smb-ui/vite.config.ts
+++ smb-ui/vite.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
+// WARNING: React throws this error as a warning. It can be safely ignored.
+// @see https://github.com/vitejs/vite/issues/16405
+// https://github.com/RJ/tachyon/issues/10
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
